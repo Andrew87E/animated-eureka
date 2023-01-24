ef23e3c0c4b07fec7957b78f9333817d30d4d1db
Andrew <Andrew@Edwards.codes>
2023-01-24 11:04:39 -0600

    initial commit

---
+++ renderer/pages/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+
+function Home() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>App Title</title>
+      </Head>
+      <div>
+      
+
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Home;
